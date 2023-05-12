b67da595e7b0a2879e7944cb18f8bdda9ea7a59e
Mergim Halimi <m.halimi123@gmail.com>
2023-05-12 18:29:54 +0200

    Added doxygen find package.

---
+++ third_party/third_party.cmake
@@ -0,0 +1,7 @@
+cmake_minimum_required(VERSION 3.15)
+
+# FetchContent is used to clone and configure third_party libraries.
+include(FetchContent)
+
+# Third-party dependencies available
+include(doxygen)
